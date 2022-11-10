617c7d0fda49355570e51a45d3a62383239bd407
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-11-10 17:21:58 +0000

    Add project files.

---
+++ PassByReferenceTest/Guy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PassByReferenceTest
+{
+    class Guy
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString() => $"a {Age}-year old named {Name}";
+    }
+}
